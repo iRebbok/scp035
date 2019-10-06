7ad498f74d80a53f433d16f9cc8a107de6525822
iRebbok <iRebbok@outlook.com>
2019-10-06 11:03:04 +0300

    adding tag in lang key & cleanup

--- scp035/Logic.cs
+++ scp035/Logic.cs
@@ -1,182 +1,178 @@
-﻿using Smod2.API;
-using Smod2.EventHandlers;
-using Smod2.Events;
-using System.Linq;
+﻿using MEC;
+using Smod2.API;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using MEC;
-using ServerMod2.API;
-using UnityEngine.Networking;
 
 namespace scp035
 {
-	partial class EventHandler
-	{
-		public static EventHandler hInstance { get; private set; }
+    partial class EventHandler
+    {
+        public static EventHandler hInstance { get; private set; }
 
-		private void LoadConfigs()
-		{
-			possibleItems = instance.GetConfigIntList("035_possible_items").ToList();
-			scpHealth = instance.GetConfigInt("035_health");
-			scpInterval = instance.GetConfigFloat("035_rotate_interval");
-			is035FriendlyFire = instance.GetConfigBool("035_scp_friendly_fire");
-			possessedItemCount = instance.GetConfigInt("035_infected_item_count");
-			spawnNewItems = instance.GetConfigBool("035_spawn_new_items");
-			useDamageOverride = instance.GetConfigBool("035_use_damage_override");
-			winWithTutorials = instance.GetConfigBool("035_win_with_tutorial");
-			changeToZombie = instance.GetConfigBool("035_change_to_zombie");
-			isTutorialFriendlyFire = instance.GetConfigBool("035_tutorial_friendly_fire");
-			isCorroding = instance.GetConfigBool("035_corrode_players");
-			corrodeRange = instance.GetConfigFloat("035_corrode_distance");
-			corrodeDamage = instance.GetConfigInt("035_corrode_damage");
-			corrodeInterval = instance.GetConfigFloat("035_corrode_interval");
-			corrodeLifeSteal = instance.GetConfigBool("035_corrode_life_steal");
-			isEnabled = instance.GetConfigBool("035_enabled");
+        private void LoadConfigs()
+        {
+            possibleItems = instance.GetConfigIntList("035_possible_items").ToList();
+            scpHealth = instance.GetConfigInt("035_health");
+            scpInterval = instance.GetConfigFloat("035_rotate_interval");
+            is035FriendlyFire = instance.GetConfigBool("035_scp_friendly_fire");
+            possessedItemCount = instance.GetConfigInt("035_infected_item_count");
+            spawnNewItems = instance.GetConfigBool("035_spawn_new_items");
+            useDamageOverride = instance.GetConfigBool("035_use_damage_override");
+            winWithTutorials = instance.GetConfigBool("035_win_with_tutorial");
+            changeToZombie = instance.GetConfigBool("035_change_to_zombie");
+            isTutorialFriendlyFire = instance.GetConfigBool("035_tutorial_friendly_fire");
+            isCorroding = instance.GetConfigBool("035_corrode_players");
+            corrodeRange = instance.GetConfigFloat("035_corrode_distance");
+            corrodeDamage = instance.GetConfigInt("035_corrode_damage");
+            corrodeInterval = instance.GetConfigFloat("035_corrode_interval");
+            corrodeLifeSteal = instance.GetConfigBool("035_corrode_life_steal");
+            isEnabled = instance.GetConfigBool("035_enabled");
 
             int ConfigBCTime = instance.GetConfigInt("035_bc_time");
             if (ConfigBCTime > -1) bctime = (uint)ConfigBCTime;
             else bctime = 10;
 
-            hiding = instance.GetTranslation("hiding");
-            picked = instance.GetTranslation("picked");
-            infection = instance.GetTranslation("infection");
+            hiding = instance.GetTranslation("035_hiding");
+            picked = instance.GetTranslation("035_picked");
+            infection = instance.GetTranslation("035_infection");
         }
 
-		private void ResetItemDurability()
-		{
-			for (int i = 0; i < scpPickups.Count; i++)
-			{
-				Pickup p = scpPickups.ElementAt(i).Key;
-				if (p != null) p.info.durability = scpPickups[p];
-			}
-			scpPickups.Clear();
-		}
+        private void ResetItemDurability()
+        {
+            for (int i = 0; i < scpPickups.Count; i++)
+            {
+                Pickup p = scpPickups.ElementAt(i).Key;
+                if (p != null) p.info.durability = scpPickups[p];
+            }
+            scpPickups.Clear();
+        }
 
-		private void RemovePossessedItems()
-		{
-			for (int i = 0; i < scpPickups.Count; i++)
-			{
-				Pickup p = scpPickups.ElementAt(i).Key;
-				if (p != null) p.Delete();
-			}
-			scpPickups.Clear();
-		}
+        private void RemovePossessedItems()
+        {
+            for (int i = 0; i < scpPickups.Count; i++)
+            {
+                Pickup p = scpPickups.ElementAt(i).Key;
+                if (p != null) p.Delete();
+            }
+            scpPickups.Clear();
+        }
 
-		private Pickup GetRandomValidItem()
-		{
-			List<Pickup> pickups = Object.FindObjectsOfType<Pickup>().Where(x => possibleItems.Contains(x.info.itemId) && !scpPickups.ContainsKey(x)).ToList();
-			return pickups[rand.Next(pickups.Count)];
-		}
+        private Pickup GetRandomValidItem()
+        {
+            List<Pickup> pickups = Object.FindObjectsOfType<Pickup>().Where(x => possibleItems.Contains(x.info.itemId) && !scpPickups.ContainsKey(x)).ToList();
+            return pickups[rand.Next(pickups.Count)];
+        }
 
-		private Pickup GetRandomItem()
-		{
-			List<Pickup> pickups = Object.FindObjectsOfType<Pickup>().Where(x => !scpPickups.ContainsKey(x)).ToList();
-			return pickups[rand.Next(pickups.Count)];
-		}
+        private Pickup GetRandomItem()
+        {
+            List<Pickup> pickups = Object.FindObjectsOfType<Pickup>().Where(x => !scpPickups.ContainsKey(x)).ToList();
+            return pickups[rand.Next(pickups.Count)];
+        }
 
-		private void RefreshItems()
-		{
-			if (instance.Server.GetPlayers().Where(x => x.TeamRole.Team == Smod2.API.Team.SPECTATOR && !x.OverwatchMode).ToList().Count > 0)
-			{
-				if (spawnNewItems)
-				{
-					RemovePossessedItems();
-					for (int i = 0; i < possessedItemCount; i++)
-					{
-						Pickup p = GetRandomItem();
-						Pickup a = PlayerManager.singleton.players[0]
-							.GetComponent<Inventory>().SetPickup(possibleItems[rand.Next(possibleItems.Count)],
-							-4.65664672E+11f,
-							new Vector3(p.transform.position.x, p.transform.position.y, p.transform.position.z),
-							new Quaternion(p.transform.rotation.x, p.transform.rotation.y, p.transform.rotation.z, p.transform.rotation.w),
-							0, 0, 0).GetComponent<Pickup>();
-						scpPickups.Add(a, a.info.durability);
-						a.info.durability = dur;
-						//new SmodItem(a.info.itemId, a).SetPosition(instance.Server.GetPlayers().FirstOrDefault(x => x.Name.Contains("cyan")).GetPosition());
-					}
-				}
-				else
-				{
-					ResetItemDurability();
-					for (int i = 0; i < possessedItemCount; i++)
-					{
-						Pickup p = GetRandomValidItem();
-						scpPickups.Add(p, p.info.durability);
-						p.info.durability = dur;
-					}
-				}
-			}
-		}
+        private void RefreshItems()
+        {
+            if (instance.Server.GetPlayers().Where(x => x.TeamRole.Team == Smod2.API.Team.SPECTATOR && !x.OverwatchMode).ToList().Count > 0)
+            {
+                if (spawnNewItems)
+                {
+                    RemovePossessedItems();
+                    for (int i = 0; i < possessedItemCount; i++)
+                    {
+                        Pickup p = GetRandomItem();
+                        Pickup a = PlayerManager.singleton.players[0]
+                            .GetComponent<Inventory>().SetPickup(possibleItems[rand.Next(possibleItems.Count)],
+                            -4.65664672E+11f,
+                            new Vector3(p.transform.position.x, p.transform.position.y, p.transform.position.z),
+                            new Quaternion(p.transform.rotation.x, p.transform.rotation.y, p.transform.rotation.z, p.transform.rotation.w),
+                            0, 0, 0).GetComponent<Pickup>();
+                        scpPickups.Add(a, a.info.durability);
+                        a.info.durability = dur;
+                        //new SmodItem(a.info.itemId, a).SetPosition(instance.Server.GetPlayers().FirstOrDefault(x => x.Name.Contains("cyan")).GetPosition());
+                    }
+                }
+                else
+                {
+                    ResetItemDurability();
+                    for (int i = 0; i < possessedItemCount; i++)
+                    {
+                        Pickup p = GetRandomValidItem();
+                        scpPickups.Add(p, p.info.durability);
+                        p.info.durability = dur;
+                    }
+                }
+            }
+        }
 
-		private void KillScp035(bool setRank = true)
-		{
-			if (setRank) scpPlayer.SetRank("default", " ");
-			scpPlayer = null;
-			isRotating = true;
-			RefreshItems();
-		}
+        private void KillScp035(bool setRank = true)
+        {
+            if (setRank) scpPlayer.SetRank("default", " ");
+            scpPlayer = null;
+            isRotating = true;
+            RefreshItems();
+        }
 
-		private void InfectPlayer(Player player, Smod2.API.Item pItem)
-		{
-			List<Player> pList = instance.Server.GetPlayers().Where(x => x.TeamRole.Team == Smod2.API.Team.SPECTATOR && !x.OverwatchMode).ToList();
-			if (pList.Count > 0 && scpPlayer == null)
-			{
-				pItem.Remove();
-				Player p035 = pList[rand.Next(pList.Count)];
-				p035.ChangeRole(player.TeamRole.Role);
-				p035.Teleport(player.GetPosition());
-				foreach (Smod2.API.Item item in player.GetInventory()) p035.GiveItem(item.ItemType);
-				p035.SetHealth(scpHealth);
-				p035.SetAmmo(AmmoType.DROPPED_5, player.GetAmmo(AmmoType.DROPPED_5));
-				p035.SetAmmo(AmmoType.DROPPED_7, player.GetAmmo(AmmoType.DROPPED_7));
-				p035.SetAmmo(AmmoType.DROPPED_9, player.GetAmmo(AmmoType.DROPPED_9));
-				p035.SetRank("red", "SCP-035");
-				p035.PersonalBroadcast(bctime, infection, false);
-				scpPlayer = p035;
-				isRotating = false;
+        private void InfectPlayer(Player player, Smod2.API.Item pItem)
+        {
+            List<Player> pList = instance.Server.GetPlayers().Where(x => x.TeamRole.Team == Smod2.API.Team.SPECTATOR && !x.OverwatchMode).ToList();
+            if (pList.Count > 0 && scpPlayer == null)
+            {
+                pItem.Remove();
+                Player p035 = pList[rand.Next(pList.Count)];
+                p035.ChangeRole(player.TeamRole.Role);
+                p035.Teleport(player.GetPosition());
+                foreach (Smod2.API.Item item in player.GetInventory()) p035.GiveItem(item.ItemType);
+                p035.SetHealth(scpHealth);
+                p035.SetAmmo(AmmoType.DROPPED_5, player.GetAmmo(AmmoType.DROPPED_5));
+                p035.SetAmmo(AmmoType.DROPPED_7, player.GetAmmo(AmmoType.DROPPED_7));
+                p035.SetAmmo(AmmoType.DROPPED_9, player.GetAmmo(AmmoType.DROPPED_9));
+                p035.SetRank("red", "SCP-035");
+                p035.PersonalBroadcast(bctime, infection, false);
+                scpPlayer = p035;
+                isRotating = false;
 
-				player.ChangeRole(Role.SPECTATOR);
-				player.PersonalBroadcast(bctime, picked, false);
+                player.ChangeRole(Role.SPECTATOR);
+                player.PersonalBroadcast(bctime, picked, false);
 
-				if (spawnNewItems) RemovePossessedItems(); else ResetItemDurability();
-			}
-		}
+                if (spawnNewItems) RemovePossessedItems(); else ResetItemDurability();
+            }
+        }
 
-		private IEnumerator<float> RotatePickup()
-		{
-			while (isRoundStarted)
-			{
-				if (isRotating)
-				{
-					RefreshItems();
-				}
-				yield return Timing.WaitForSeconds(scpInterval);
-			}
-		}
+        private IEnumerator<float> RotatePickup()
+        {
+            while (isRoundStarted)
+            {
+                if (isRotating)
+                {
+                    RefreshItems();
+                }
+                yield return Timing.WaitForSeconds(scpInterval);
+            }
+        }
 
-		private void CorrodePlayer(Player player)
-		{
-			if (useDamageOverride)
-			{
-				player.SetHealth(player.GetHealth() - corrodeDamage, DamageType.NONE);
-			}
-			else
-			{
-				player.Damage(corrodeDamage, DamageType.NONE);
-			}
+        private void CorrodePlayer(Player player)
+        {
+            if (useDamageOverride)
+            {
+                player.SetHealth(player.GetHealth() - corrodeDamage, DamageType.NONE);
+            }
+            else
+            {
+                player.Damage(corrodeDamage, DamageType.NONE);
+            }
 
-			if (corrodeLifeSteal && scpPlayer != null)
-			{
-				int currHP = scpPlayer.GetHealth();
-				scpPlayer.SetHealth(currHP + corrodeDamage > scpHealth ? scpHealth : currHP + corrodeDamage);
-			}
-		}
+            if (corrodeLifeSteal && scpPlayer != null)
+            {
+                int currHP = scpPlayer.GetHealth();
+                scpPlayer.SetHealth(currHP + corrodeDamage > scpHealth ? scpHealth : currHP + corrodeDamage);
+            }
+        }
 
-		public bool HandleHideTagHook(CharacterClassManager __instance)
-		{
-			bool a = __instance.SteamId == scpPlayer?.SteamId;
-			if (a) __instance.TargetConsolePrint(__instance.connectionToClient, hiding, "green");
-			return !a;
-		}
-	}
+        public bool HandleHideTagHook(CharacterClassManager __instance)
+        {
+            bool a = __instance.SteamId == scpPlayer?.SteamId;
+            if (a) __instance.TargetConsolePrint(__instance.connectionToClient, hiding, "green");
+            return !a;
+        }
+    }
 }
--- scp035/Plugin.cs
+++ scp035/Plugin.cs
@@ -1,54 +1,54 @@
-﻿using Smod2.Attributes;
-using Harmony;
+﻿using Harmony;
+using Smod2.Attributes;
 
 namespace scp035
 {
-	[PluginDetails(
-	author = "Cyanox",
-	name = "SCP-035",
-	description = "Adds SCP-035 to the game.",
-	id = "cyan.scp035",
-	version = "1.5.4",
-	SmodMajor = 3,
-	SmodMinor = 0,
-	SmodRevision = 0
-	)]
-	public class Plugin : Smod2.Plugin
-	{
-		public override void OnDisable() { }
+    [PluginDetails(
+    author = "Cyanox",
+    name = "SCP-035",
+    description = "Adds SCP-035 to the game.",
+    id = "cyan.scp035",
+    version = "1.5.4",
+    SmodMajor = 3,
+    SmodMinor = 0,
+    SmodRevision = 0
+    )]
+    public class Plugin : Smod2.Plugin
+    {
+        public override void OnDisable() { }
 
-		public override void OnEnable() { }
+        public override void OnEnable() { }
 
-		public override void Register()
-		{
-			HarmonyInstance.Create(Details.id).PatchAll();
+        public override void Register()
+        {
+            HarmonyInstance.Create(Details.id).PatchAll();
 
-			// This is for the damage override, don't AngryLaserBoi me
-			AddEventHandlers(new EventHandler(this), Smod2.Events.Priority.High);
+            // This is for the damage override, don't AngryLaserBoi me
+            AddEventHandlers(new EventHandler(this), Smod2.Events.Priority.High);
 
-			AddConfig(new Smod2.Config.ConfigSetting("035_possible_items", new[] {
-				0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 23, 24, 25, 26, 30
-			}, false, true, "The possible item IDs SCP-035 can disguise as."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_health", 300, false, true, "The amount of health SCP-035 should have."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_rotate_interval", 120f, false, true, "The time in seconds before the item representing SCP-035 will refresh."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_scp_friendly_fire", false, false, true, "If SCP-035 is allowed to damage other SCPs."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_infected_item_count", 1, false, true, "The number of items at a time that are possessed by SCP-035."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_spawn_new_items", false, false, true, "If the plugin should spawn a new item on top of a randomly selected item rather than possessing already existing items."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_use_damage_override", false, false, true, "If the plugin should override AdmintoolBox's damage blockers."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_win_with_tutorial", false, false, true, "If SCP-035 should win with tutorials."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_change_to_zombie", false, false, true, "If SCP-035 should change to a zombie when the SCP team should win. This may fix round lingering issues due to conflicting end conditions."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_tutorial_friendly_fire", false, false, true, "If friendly fire between SCP-035 and tutorials is enabled."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_corrode_players", true, false, true, "If SCP-035 should do passive damage to players within a range of him."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_corrode_distance", 1.5f, false, true, "The distance in which a player will take corrosion damage from SCP-035."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_corrode_damage", 5, false, true, "The amount of damage to do to a player within range of corrosion."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_corrode_interval", 1f, false, true, "The interval in seconds for corrosion damage."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_corrode_life_steal", true, false, true, "If SCP-035 should steal any health taken from other players by corrosion."));
-			AddConfig(new Smod2.Config.ConfigSetting("035_enabled", true, false, true, "If SCP-035 is enabled."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_possible_items", new[] {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 23, 24, 25, 26, 30
+            }, false, true, "The possible item IDs SCP-035 can disguise as."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_health", 300, false, true, "The amount of health SCP-035 should have."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_rotate_interval", 120f, false, true, "The time in seconds before the item representing SCP-035 will refresh."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_scp_friendly_fire", false, false, true, "If SCP-035 is allowed to damage other SCPs."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_infected_item_count", 1, false, true, "The number of items at a time that are possessed by SCP-035."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_spawn_new_items", false, false, true, "If the plugin should spawn a new item on top of a randomly selected item rather than possessing already existing items."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_use_damage_override", false, false, true, "If the plugin should override AdmintoolBox's damage blockers."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_win_with_tutorial", false, false, true, "If SCP-035 should win with tutorials."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_change_to_zombie", false, false, true, "If SCP-035 should change to a zombie when the SCP team should win. This may fix round lingering issues due to conflicting end conditions."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_tutorial_friendly_fire", false, false, true, "If friendly fire between SCP-035 and tutorials is enabled."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_corrode_players", true, false, true, "If SCP-035 should do passive damage to players within a range of him."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_corrode_distance", 1.5f, false, true, "The distance in which a player will take corrosion damage from SCP-035."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_corrode_damage", 5, false, true, "The amount of damage to do to a player within range of corrosion."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_corrode_interval", 1f, false, true, "The interval in seconds for corrosion damage."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_corrode_life_steal", true, false, true, "If SCP-035 should steal any health taken from other players by corrosion."));
+            AddConfig(new Smod2.Config.ConfigSetting("035_enabled", true, false, true, "If SCP-035 is enabled."));
 
             AddConfig(new Smod2.Config.ConfigSetting("035_bc_time", 10, true, "Broadcast time"));
-            AddTranslation(new Smod2.Lang.LangSetting("infection", "You are <color=red>SCP-035!</color> You have infected a body and have gained control over it, use it to help the other SCPs!", "SCP035"));
-            AddTranslation(new Smod2.Lang.LangSetting("picked", "You have picked up <color=red>SCP-035.</color> He has infected your body and is now in control of you.", "SCP035"));
-            AddTranslation(new Smod2.Lang.LangSetting("hiding", "You're not trying to exploit the system by hiding your tag as SCP-035 now, are you?", "SCP035)"));
+            AddTranslation(new Smod2.Lang.LangSetting("035_infection", "You are <color=red>SCP-035!</color> You have infected a body and have gained control over it, use it to help the other SCPs!", "SCP035"));
+            AddTranslation(new Smod2.Lang.LangSetting("035_picked", "You have picked up <color=red>SCP-035.</color> He has infected your body and is now in control of you.", "SCP035"));
+            AddTranslation(new Smod2.Lang.LangSetting("035_hiding", "You're not trying to exploit the system by hiding your tag as SCP-035 now, are you?", "SCP035)"));
         }
-	}
+    }
 }
